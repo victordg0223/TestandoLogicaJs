421e98b107cb29460e3439bc01935e2d9260ca81
Victor D. G. <victordg0223@gmail.com>
2025-10-02 21:24:16 -0300

    feito exercicio junto da aula

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
-let numeroSecreto = 4;
-
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
-if (chute == numeroSecreto) {
-    alert('Acertou');
-} else {
-    // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-    console.log('Valor do número secreto:', numeroSecreto);
-    alert('O número secreto era ' + numeroSecreto);
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+let chute;
+let numChute = 1;
+while (chute != numeroSecreto){
+    chute = prompt("Escolhga um numero de 1 a 10");
+    if (chute == numeroSecreto){
+        alert("boa voce acertou");
+    } else {
+        numChute++;
+        if (numeroSecreto > chute){
+        alert(`O numero secreto e maior que o ${chute}`);
+        } else {
+        alert(`O numero secreto e menor que ${chute}`);
+        }
+    }
 }
+alert(numChute);
